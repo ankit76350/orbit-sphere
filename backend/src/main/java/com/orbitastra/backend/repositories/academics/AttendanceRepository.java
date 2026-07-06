dd23f798473828fed7458ebb9d4971eefd2af96f
Ankit Kumar <krankit01012003@gmail.com>
2026-07-06 15:17:01 +0530

    We have aleadry completed the attendence api

---
+++ backend/src/main/java/com/orbitastra/backend/repositories/academics/AttendanceRepository.java
@@ -0,0 +1,17 @@
+package com.orbitastra.backend.repositories.academics;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.orbitastra.backend.models.academics.Attendance;
+
+@Repository
+public interface AttendanceRepository extends MongoRepository<Attendance, String> {
+    List<Attendance> findBySchoolId(String schoolId);
+    List<Attendance> findByStudentId(String studentId);
+    List<Attendance> findByStudentIdAndDate(String studentId, LocalDate date);
+    List<Attendance> findBySchoolIdAndDate(String schoolId, LocalDate date);
+}
