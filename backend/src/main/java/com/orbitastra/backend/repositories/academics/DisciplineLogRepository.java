55a2f62f00e27623c582a94edc62a18d21dd9f4b
Ankit Kumar <krankit01012003@gmail.com>
2026-07-06 15:32:41 +0530

    completed the disicple repsosity for the api and api is aleadry completed

---
+++ backend/src/main/java/com/orbitastra/backend/repositories/academics/DisciplineLogRepository.java
@@ -0,0 +1,14 @@
+package com.orbitastra.backend.repositories.academics;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.orbitastra.backend.models.academics.DisciplineLog;
+
+@Repository
+public interface DisciplineLogRepository extends MongoRepository<DisciplineLog, String> {
+    List<DisciplineLog> findBySchoolId(String schoolId);
+    List<DisciplineLog> findByStudentId(String studentId);
+}
