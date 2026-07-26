eaeb16b68018b45ae6e39bdbb607ff183e52382e
Ankit Kumar <krankit01012003@gmail.com>
2026-07-26 12:46:57 +0530

    feat: create Exam model to manage examination scheduling and datesheets

--- backend/src/main/java/com/orbitastra/backend/models/undone/exams/Exam.java
+++ backend/src/main/java/com/orbitastra/backend/models/undone/exams/Exam.java
@@ -5,7 +5,6 @@ import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.index.Indexed;
